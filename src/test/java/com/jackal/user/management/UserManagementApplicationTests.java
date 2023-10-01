f437cc57a3cec82a37d7c295f65aa7289134eca8
Talha Çakal <talha.cakal1@gmail.com>
2023-10-01 15:48:39 +0300

    Spring Project Created

---
+++ src/test/java/com/jackal/user/management/UserManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jackal.user.management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
